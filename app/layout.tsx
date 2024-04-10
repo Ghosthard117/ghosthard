f59cc0dd682a21b60068bbcaf442d20589646295
MIguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2024-04-09 20:07:30 -0600

    "Add Prismic preview and update metadata generation with data from Prismic"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import { PrismicPreview } from "@prismicio/next";
+import { createClient, repositoryName } from "@/prismicio";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import clsx from "clsx";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Ghosthard portfolio",
-  description: "My personal portfolio",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return {
+    title: settings.data.meta_tittle,
+    description: settings.data.meta_description,
+  };
+}
 
 export default function RootLayout({
   children,
@@ -17,13 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-slate-900 text-slate-100">
-      <body className={urbanist.className}>
+    <html lang="en" className="bg-slate-900">
+      <body className={clsx(urbanist.className, "relative min-h-screen")}>
         <Header />
         {children}
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
         <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
